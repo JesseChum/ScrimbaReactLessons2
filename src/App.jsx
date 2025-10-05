4d3d72d9a6390f43d97c4655b60c26ca9197bd45
JesseChum <jessechum@icloud.com>
2025-10-04 19:14:19 -0700

    Got images to work and fixed css. finished main project my travel journal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import data from "./data"
 
 export default function App() {
+    
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry
+                key={entry.id}
+                {...entry}
+            />
+        )
+    })
+    
     return (
         <>
             <Header />
             <main className="container">
-                <Entry />
+                {entryElements}
             </main>
         </>
     )
